595e63c3e58adc710d4c4c88aca59d35546a8844
cage433 <cage433@gmail.com>
2021-09-16 10:29:23 +0100

    remove commented out section

--- google-sheets/volunteer-sheet/model.rb
+++ google-sheets/volunteer-sheet/model.rb
@@ -9,44 +9,6 @@ def assert_dimension_2d(arr, expected_rows, expected_cols)
 end
 
 
-#class VolunteerSheetDetailsForEvent
-  #attr_reader :event_date
-  #def initialize(event_date, event_title, personnel)
-    #@event_date = event_date
-    #@event_title = event_title
-    #@personnel = personnel
-  #end
-
-  #def to_excel_data()
-    #[
-      #[@event_title, @event_date, @event_date, 1, "19:00"] + 
-        #SetPersonnelMediator.to_excel_data(@personnel.first_set_volunteer_data) + 
-        #[@personnel.sound_engineer],
-      #["", "", "", 2, "21:00"] + 
-      #SetPersonnelMediator.to_excel_data(@personnel.second_set_volunteer_data) + [""]
-    #]
-  #end
-
-  #def self.from_airtable_record(record)
-    #VolunteerSheetDetailsForEvent.new(record.event_date, record.event_title, GigPersonnel.empty)
-  #end
-
-  #def self.from_excel(rows)
-    #assert_dimension_2d(rows, 2, 9)
-    #event_date = Date.parse(rows[0][1])
-    #event_title = rows[0][0]
-    #personnel = GigPersonnelMediator.from_excel(
-      #[
-        #rows[0].slice(5, 4),
-        #rows[1].slice(5, 4)
-      #]
-    #)
-    #VolunteerSheetDetailsForEvent.new(
-      #event_date, event_title, personnel
-    #)
-  #end
-
-#end
 
 class VolunteerSheetDetailsForMonth
   def initialize(event_details)
