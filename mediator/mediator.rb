2e42e89fc9cd3e2fd5967a94a8b5065f075a24ee
cage433 <cage433@gmail.com>
2021-09-20 07:11:08 +0100

    wip

--- mediator/mediator.rb
+++ mediator/mediator.rb
@@ -4,26 +4,6 @@ require_relative '../airtable/gig_table'
 
 require 'date'
 
-def assert_dimension_2d(arr, expected_rows, expected_cols)
-  raise "Row dimension mismatch, expected #{expected_rows}, got #{arr.size}" if arr.size != expected_rows
-  arr.each do |row|
-  raise "Col dimension mismatch, expected #{expected_cols}, got #{row.size}" if row.size != expected_cols
-  end
-end
-
-class GigMediator
-  include GigTableMeta
-  def self.from_airtable_record(rec)
-    Gig.new(
-      airtable_id: rec[ID], 
-      gig_no: rec[GIG_NO], 
-      vol1: rec[VOL_1],
-      vol2: rec[VOL_2],
-      night_manager: rec[NIGHT_MANAGER]
-    )
-  end
-
-end
 
 class GigTakingsMediator
   include GigTableMeta
@@ -70,33 +50,6 @@ class EventMediator
       ]
       blank_row = [""] * 20
       [first_row, second_row, blank_row]
-  end
-
-  def self.from_airtable_many(event_ids)
-    include EventTableMeta
-    event_records = EventTable.find_many(event_ids)
-
-    gig_ids = event_records.collect { |rec| rec[GIG_IDS] }.flatten
-    gigs_by_id = Hash[ 
-      GigTable
-      .find_many(gig_ids)
-      .collect { |rec| 
-        [rec[ID], GigMediator.from_airtable_record(rec)] 
-      } 
-    ]
-    event_records.collect { |event_record|
-      gig1, gig2 = event_record[GIG_IDS].collect { |id| gigs_by_id[id] }.sort_by{ |g| g.gig_no }
-      event_date = Date.parse(event_record[DATE])
-      event_title = event_record[TITLE]
-      Event.new(
-        airtable_id: event_record[ID],
-        event_date: event_date,
-        event_title: event_record[TITLE],
-        gig1: gig1, gig2: gig2,
-        sound_engineer: event_record[SOUND_ENGINEER]
-      )
-    }
-
   end
 
 
