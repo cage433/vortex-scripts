08ca2a6c5e929ba7b0ebc4d0509f71b3473fa4de
cage433 <cage433@gmail.com>
2021-11-02 04:52:30 +0000

    Remove redundant files

--- controller.rb
+++ controller.rb
@@ -19,12 +19,6 @@ class Controller
     VolunteerMonthTabController.new(year, month, @vol_rota_controller)
   end
 
-  def night_manager_tab_controller(date)
-    tab_name = TabController.tab_name_for_date(date)
-    @night_manager_controller.add_tab(tab_name) if !@night_manager_controller.has_tab_with_name?(tab_name)
-    NightManagerTabController2.new(date, @night_manager_controller)
-  end
-
   def update_vol_sheet_from_airtable(year, month, force)
     tab_controller = vol_tab_controller(year, month)
     airtable_events_personnel = VolunteerAirtableController.read_events_personnel(year, month)
@@ -62,52 +56,6 @@ class Controller
 
   end
 
-  def update_nm_tab_from_airtable2(date, force)
-    # Add new events and update any modified prices
-    tab_controller = night_manager_tab_controller2(date)
-    sheet_details = tab_controller.read_details()
-    airtable_details = NightManagerAirtableController.read_events_for_date(date)
-    events = airtable_events.diff_by_date(events) + events
-
-    if sheet_details != airtable_details || force
-      tab_controller.update_details_from_airtable(airtable_details)
-    end
-  end
-
-  def update_nm_tab_from_airtable(year, month, force)
-    # Add new events and update any modified prices
-    tab_controller = night_manager_tab_controller(year, month)
-    original_events = tab_controller.read_events()
-
-    events = tab_controller.read_events()
-    airtable_events = NightManagerAirtableController.read_events_for_month(year, month)
-    events = airtable_events.diff_by_date(events) + events
-
-    airtable_events.data.each { |a|
-      e = events[a.date]
-      e.update_gig1_ticket_price(a.gig1_takings.ticket_price)
-      e.update_gig2_ticket_price(a.gig2_takings.ticket_price)
-      e.update_fee_details(a.fee_details)
-    }
-
-    if events != original_events || force
-      tab_controller.replace_events(events)
-    end
-
-  end
-
-  def update_airtable_from_nm_tab(year, month, force)
-    tab_controller = night_manager_tab_controller(year, month)
-    tab_events = tab_controller.read_events()
-    if force
-      NightManagerAirtableController.update_events(tab_events.data)
-    else
-      airtable_events = NightManagerAirtableController.read_events_for_month(year, month)
-      modified_events = tab_events.changed_data(airtable_events)
-      NightManagerAirtableController.update_events(modified_events.data)
-    end
-  end
-end
 
 def sync_personnel_data(year, month, force = false)
   controller = Controller.new()
@@ -115,21 +63,6 @@ def sync_personnel_data(year, month, force = false)
   controller.update_airtable_from_vol_sheet(year, month, force)
 end
 
-def populate_new_event_table(year, month)
-  EventTable.populate_for_date_range(
-    Date.new(year, month, 1),
-    Date.new(year, month, -1)
-  )
-end
-
-def sync_night_manager_data(year, month, force = false)
-  controller = Controller.new()
-  controller.update_nm_tab_from_airtable(year, month, force)
-  controller.update_airtable_from_nm_tab(year, month, force)
-end
-
-#populate_new_event_table(2021, 10)
 
 #sync_personnel_data(2021, 10, force=false)
 
-sync_night_manager_data(2021, 10, force=false)
--- google-sheets/night_controller.rb
+++
@@ -1,4 +0,0 @@
-module NightManagerCells
-  DATE_CELL = [1, 1]
-  TITLE_CELL = [2, 1]
-end
