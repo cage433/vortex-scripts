5e4fc6e717c7ea8b1433dc5138c90236670d6562
cage433 <cage433@gmail.com>
2021-11-28 18:50:23 +0000

    all nm ranges

--- google-sheets/nm_tab_controller.rb
+++ google-sheets/nm_tab_controller.rb
@@ -355,6 +355,115 @@ class PRSRange < TabController
   end
 end
 
+class ZReadingsRange < TabController
+  def initialize(wb_controller, range, tab_name)
+    super(wb_controller, tab_name)
+    @range = range
+    @header = @range.row(0)
+    @readings_range = @range.column(1).rows(2..)
+    @cash_reading_cell = @readings_range.cell(0)
+    @zettle_reading_cell = @readings_range.cell(1)
+  end
+
+  def initialise_range()
+    @wb_controller.set_data(
+      @range.column(0),
+      ["Z Readings", "", "Cash (£)", "Zettle (£)"]
+    )
+    amounts_range = @range.rows(2..3).column(1)
+    requests = [
+      set_outside_border_request(@range),
+      set_border_request(@header, style: "SOLID", borders: [:bottom]),
+      merge_columns_request(@header),
+      bold_and_center_request(@range.column(0)),
+      set_currency_format_request(@readings_range),
+      set_background_color_request(@readings_range, @@almond),
+    ]
+    @wb_controller.apply_requests(requests)
+  end
+
+  def zettle_reading_value()
+    get_cell_value(@zettle_reading_cell)
+  end
+
+  def cash_reading_value()
+    get_cell_value(@cash_reading_cell)
+  end
+
+  def set_z_readings(session_data)
+    @wb_controller.set_data(
+      @readings_range,
+      [session_data.cash_z_reading, session_data.zettle_z_reading]
+    )
+  end
+end
+
+class MerchRange < TabController
+  def initialize(wb_controller, range, tab_name)
+    super(wb_controller, tab_name)
+    @range = range
+    @input_range = @range.rows(2..).columns(1..)
+  end
+
+  def initialise_range()
+    @wb_controller.set_data(
+      @range.column(0).rows(2..5),
+      ["Mugs", "T-shirts", "Bags", "Masks"]
+    )
+    @wb_controller.set_data(
+      @range.row(1).columns(1..2),
+      ["Number", "Amount (£)"]
+    )
+    titles_range = @range.rows(0..1)
+    requests = [
+      set_outside_border_request(@range),
+      set_border_request(@range.row(1), style: "SOLID", borders: [:bottom]),
+      set_border_request(@range.column(0).rows(1..), style: "SOLID", borders: [:right]),
+      merge_columns_request(@range.row(0)),
+      bold_and_center_request(titles_range),
+      bold_text_request(@range.column(0)),
+      set_background_color_request(@input_range, @@almond),
+      set_currency_format_request(@input_range.column(1)),
+    ]
+    @wb_controller.apply_requests(requests)
+
+  end
+
+  def number_and_value(i_row)
+    row = @input_range.row(i_row)
+    NumberSoldAndValue.new(
+      number: get_cell_value(row.cell(0)), 
+      value: get_cell_value(row.cell(1))
+    )
+  end
+
+  def mugs_number_and_value()
+    number_and_value(0)
+  end
+
+  def t_shirts_number_and_value()
+    number_and_value(1)
+  end
+
+  def bags_number_and_value()
+    number_and_value(2)
+  end
+
+  def masks_number_and_value()
+    number_and_value(3)
+  end
+
+  def set_merch_data(session_data)
+    data = [session_data.mugs, session_data.t_shirts, session_data.bags, session_data.masks].collect { |item|
+      [item.number, item.value]
+    }
+    @wb_controller.set_data(
+      @input_range,
+      data
+    )
+  end
+end
+
 class NightManagerTabController < TabController
   def initialize(date, wb_controller)
     super(wb_controller, TabController.tab_name_for_date(date))
@@ -374,58 +483,11 @@ class NightManagerTabController < TabController
     )
     @expenses_range = ExpensesRange.new(@wb_controller, sheet_range_from_coordinates("H5:L10"), @tab_name)
     @prs_range = PRSRange.new(@wb_controller, sheet_range_from_coordinates("H12:I15"), @tab_name, @ticket_sales_range)
-    @z_readings_range = sheet_range_from_coordinates("K12:L15")
-    @merch_range = sheet_range_from_coordinates("H17:J22")
+    @z_readings_range = ZReadingsRange.new(@wb_controller, sheet_range_from_coordinates("K12:L15"), @tab_name)
+    @merch_range = MerchRange.new(@wb_controller, sheet_range_from_coordinates("H17:J22"), @tab_name)
   end
 
 
-
-
-
-
-
-  def build_z_readings_range()
-    @wb_controller.set_data(
-      @z_readings_range.column(0),
-      ["Z Readings", "", "Cash (£)", "Zettle (£)"]
-    )
-    amounts_range = @z_readings_range.rows(2..3).column(1)
-    requests = [
-      set_outside_border_request(@z_readings_range),
-      set_border_request(@z_readings_range.row(0), style: "SOLID", borders: [:bottom]),
-      merge_columns_request(@z_readings_range.row(0)),
-      bold_and_center_request(@z_readings_range.column(0)),
-      set_currency_format_request(amounts_range),
-      set_background_color_request(amounts_range, @@almond),
-    ]
-    @wb_controller.apply_requests(requests)
-
-  end
-
-  def build_merch_range()
-    @wb_controller.set_data(
-      @merch_range.column(0).rows(2..5),
-      ["Mugs", "T-shirts", "Bags", "Masks"]
-    )
-    @wb_controller.set_data(
-      @merch_range.row(1).columns(1..2),
-      ["Number", "Amount (£)"]
-    )
-    input_range = @merch_range.rows(2..).columns(1..)
-    titles_range = @merch_range.rows(0..1)
-    requests = [
-      set_outside_border_request(@merch_range),
-      set_border_request(@merch_range.row(1), style: "SOLID", borders: [:bottom]),
-      set_border_request(@merch_range.column(0).rows(1..), style: "SOLID", borders: [:right]),
-      merge_columns_request(@merch_range.row(0)),
-      bold_and_center_request(titles_range),
-      bold_text_request(@merch_range.column(0)),
-      set_background_color_request(input_range, @@almond),
-      set_currency_format_request(input_range.column(1)),
-    ]
-    @wb_controller.apply_requests(requests)
-
-  end
 
 
   def size_columns()
@@ -444,29 +506,18 @@ class NightManagerTabController < TabController
     @prs_range.initialise_range()
     @notes_range.initialise_range()
     @expenses_range.initialise_range()
-    build_z_readings_range()
-    build_merch_range()
+    @z_readings_range.initialise_range()
+    @merch_range.initialise_range()
   end
 
   def read_session_data()
-    merch_data = get_spreadsheet_values(@merch_range.rows(2..).columns(1..)) || []
-    def number_and_value(merch_data, i_row)
-      if i_row >= merch_data.size
-        NumberSoldAndValue.new(number: 0, value: 0)
-      else
-        NumberSoldAndValue.new(
-          number: merch_data[i_row][0],
-          value: merch_data[i_row][1]
-        )
-      end
-    end
     NMForm_SessionData.new(
-      mugs: number_and_value(merch_data, 0),
-      t_shirts: number_and_value(merch_data, 1),
-      bags: number_and_value(merch_data, 2),
-      masks: number_and_value(merch_data, 3),
-      zettle_z_reading: get_cell_value(@z_readings_range.cell(3, 1)),
-      cash_z_reading: get_cell_value(@z_readings_range.cell(2, 1)),
+      mugs: @merch_range.mugs_number_and_value(),
+      t_shirts: @merch_range.t_shirts_number_and_value(),
+      bags: @merch_range.bags_number_and_value(),
+      masks: @merch_range.masks_number_and_value(),
+      zettle_z_reading: @z_readings_range.zettle_reading_value(),
+      cash_z_reading: @z_readings_range.cash_reading_value(),
       notes: @notes_range.read_merged_note(),
       fee_to_pay: @fee_range.fee_to_pay_value,
       fully_improvised: @prs_range.fully_improvised(),
@@ -489,6 +540,8 @@ class NightManagerTabController < TabController
     @expenses_range.write_expenses(form_data.expenses_data)
     @ticket_sales_range.write_ticket_sales(form_data.ticket_sales)
     @prs_range.set_fully_improvised(form_data.session_data.fully_improvised)
+    @z_readings_range.set_z_readings(form_data.session_data)
+    @merch_range.set_merch_data(form_data.session_data)
   end
 
 end
