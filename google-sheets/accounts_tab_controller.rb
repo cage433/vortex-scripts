9bfccf374ef07ee4fc6068a004febeac4136e791
Alex McGuire <cage433@gmail.com>
2022-09-10 07:02:16 +0100

    refactoring

--- google-sheets/accounts_tab_controller.rb
+++ google-sheets/accounts_tab_controller.rb
@@ -121,6 +121,7 @@ class AccountsTabController < TabController
       @wb_controller.set_data(@sheet_range[TOTAL_INCOME_ROW, 0..(@num_weeks + 1)], values)
 
     end
+
     def set_outgoings()
       @wb_controller.set_data(@sheet_range.cell(OUTGOINGS_HEADING_ROW, 0), "Outgoings")
       @wb_controller.set_data(@sheet_range[OUTGOING_WEEK_ROW, 0..(@num_weeks + 2)], ["Week"] + @month.weeks.collect { |w| w.week_number } + ["MTD", "VAT estimate"])
@@ -155,7 +156,7 @@ class AccountsTabController < TabController
     end
 
     def set_net_values()
-      @wb_controller.set_data(@sheet_range.cell(NET_HEADING_ROW, 0), "Net P&L")
+      @wb_controller.set_data(@sheet_range.cell(NET_HEADING_ROW, 0), "Incoming - Outgoing")
       @wb_controller.set_data(@sheet_range[NET_WEEK_ROW, 0..(@num_weeks + 2)], ["Week"] + @month.weeks.collect { |w| w.week_number } + ["MTD", "VAT estimate"])
       values = (1..(@num_weeks + 1)).collect { |i|
         total_incomes_cell = @sheet_range[TOTAL_INCOME_ROW, i]
@@ -195,7 +196,24 @@ class AccountsTabController < TabController
 
 
 
+    def common_formats(range)
+      [
+        merge_columns_request(range.row(0)),
+        center_text_request(range.row(0)),
+        set_outside_border_request(range),
+        bold_text_request(range.row(0)),
+        bold_text_request(range.row(2)),
+        bold_text_request(range.row(-1)),
+        set_right_border_request(range.column(0).rows(2..)),
+        right_align_text_request(range[2, 1 + @num_weeks..]),
+      ]
+
+    end
     requests = delete_all_group_rows_requests()
+    requests += common_formats(@sheet_range[AUDIENCE_HEADING_ROW..GUESTS_ROW, 0..(@num_weeks + 1)])
+    requests += common_formats(@sheet_range[INCOMING_HEADING_ROW..TOTAL_INCOME_ROW, 0..(@num_weeks + 2)])
+    requests += common_formats(@sheet_range[OUTGOINGS_HEADING_ROW..TOTAL_OUTGOINGS_ROW, 0..(@num_weeks + 2)])
+    requests += common_formats(@sheet_range[NET_HEADING_ROW..NET_TOTAL_ROW, 0..(@num_weeks + 2)])
     requests += [
       set_column_width_request(0, 20),
       set_column_width_request(1, 150),
@@ -205,31 +223,20 @@ class AccountsTabController < TabController
       set_outside_border_request(@sheet_range[MONTH_ROW..VAT_ROW, 0..1]),
       bold_text_request(@sheet_range[MONTH_ROW..VAT_ROW, 0]),
 
-      merge_columns_request(@sheet_range[AUDIENCE_HEADING_ROW, 0..(1 + @num_weeks)]),
-      center_text_request(@sheet_range[AUDIENCE_HEADING_ROW, 0..(1 + @num_weeks)]),
-      set_outside_border_request(@sheet_range[AUDIENCE_HEADING_ROW..GUESTS_ROW, 0..(@num_weeks + 1)]),
       set_top_border_request(@sheet_range[GUESTS_ROW + 1, 0..(@num_weeks + 1)], style: "SOLID_MEDIUM"),
-      bold_text_request(@sheet_range[AUDIENCE_HEADING_ROW, 0..(@num_weeks + 1)]),
       bold_text_request(@sheet_range[AUDIENCE_WEEK_ROW..AUDIENCE_ROW, 0..(@num_weeks + 1)]),
       set_bottom_border_request(@sheet_range[AUDIENCE_WEEK_ROW, 0..(@num_weeks + 1)]),
-      set_right_border_request(@sheet_range[AUDIENCE_WEEK_ROW..GUESTS_ROW, 0]),
       set_right_border_request(@sheet_range[AUDIENCE_WEEK_ROW..GUESTS_ROW, @num_weeks]),
-      right_align_text_request(@sheet_range[AUDIENCE_WEEK_ROW, 1 + @num_weeks]),
       right_align_text_request(@sheet_range[FULL_PRICE_ROW..GUESTS_ROW, 0]),
 
-      merge_columns_request(@sheet_range[INCOMING_HEADING_ROW, 0..(2 + @num_weeks)]),
-      center_text_request(@sheet_range[INCOMING_HEADING_ROW, 0..(2 + @num_weeks)]),
       right_align_text_request(@sheet_range[FULL_PRICE_SALES_ROW..OTHER_SALES_ROW, 0]),
-      right_align_text_request(@sheet_range[INCOMING_WEEKS_ROW, (1 + @num_weeks)..(2 + @num_weeks)]),
-      set_outside_border_request(@sheet_range[INCOMING_HEADING_ROW..TOTAL_INCOME_ROW, 0..(@num_weeks + 2)]),
+      # right_align_text_request(@sheet_range[INCOMING_WEEKS_ROW, (1 + @num_weeks)..(2 + @num_weeks)]),
       set_bottom_border_request(@sheet_range[INCOMING_WEEKS_ROW, 0..(@num_weeks + 2)]),
       set_bottom_border_request(@sheet_range[CREDIT_CARD_TAKINGS_ROW, 0..(@num_weeks + 2)]),
-      set_right_border_request(@sheet_range[INCOMING_WEEKS_ROW..TOTAL_INCOME_ROW, 0]),
       set_left_right_border_request(@sheet_range[INCOMING_WEEKS_ROW..TOTAL_INCOME_ROW, 1 + @num_weeks]),
       group_rows_request(FULL_PRICE_SALES_ROW, OTHER_SALES_ROW),
       group_rows_request(FULL_PRICE_ROW, GUESTS_ROW),
       bold_text_request(@sheet_range[TOTAL_INCOME_ROW, 0..(@num_weeks + 2)]),
-      bold_text_request(@sheet_range[INCOMING_HEADING_ROW..INCOMING_WEEKS_ROW, 0..(@num_weeks + 2)]),
       bold_text_request(@sheet_range[TICKET_SALES_ROW, 0]),
       bold_text_request(@sheet_range[TOTAL_HIRE_FEES_ROW..CREDIT_CARD_TAKINGS_ROW, 0]),
       set_decimal_format_request(
@@ -239,16 +246,14 @@ class AccountsTabController < TabController
 
 
 
-      merge_columns_request(@sheet_range[OUTGOINGS_HEADING_ROW, 0..(2 + @num_weeks)]),
-      center_text_request(@sheet_range[OUTGOINGS_HEADING_ROW, 0..(2 + @num_weeks)]),
       right_align_text_request(@sheet_range[MUSICIANS_FEE_ROW..CATERING_EXPENSES_ROW, 0]),
-      set_outside_border_request(@sheet_range[OUTGOINGS_HEADING_ROW..TOTAL_OUTGOINGS_ROW, 0..(@num_weeks + 2)]),
-      right_align_text_request(@sheet_range[OUTGOING_WEEK_ROW, (1 + @num_weeks)..(2 + @num_weeks)]),
+      # right_align_text_request(@sheet_range[OUTGOING_WEEK_ROW, (1 + @num_weeks)..(2 + @num_weeks)]),
       set_bottom_border_request(@sheet_range[OUTGOING_WEEK_ROW, 0..(@num_weeks + 2)]),
       set_bottom_border_request(@sheet_range[EVENING_PURCHASES_ROW, 0..(@num_weeks + 2)]),
       set_right_border_request(@sheet_range[OUTGOING_WEEK_ROW..TOTAL_OUTGOINGS_ROW, 0]),
       set_left_right_border_request(@sheet_range[OUTGOING_WEEK_ROW..TOTAL_OUTGOINGS_ROW, 1 + @num_weeks]),
-      bold_text_request(@sheet_range[OUTGOINGS_HEADING_ROW..OUTGOING_WEEK_ROW, 0..(@num_weeks + 2)]),
+      bold_text_request(@sheet_range[TOTAL_MUSICIAN_COSTS_ROW, 0]),
+      bold_text_request(@sheet_range[PRS_ROW..EVENING_PURCHASES_ROW, 0]),
       bold_text_request(@sheet_range[TOTAL_OUTGOINGS_ROW, 0..(@num_weeks + 2)]),
       group_rows_request(MUSICIANS_FEE_ROW, CATERING_EXPENSES_ROW),
       set_decimal_format_request(
@@ -257,12 +262,8 @@ class AccountsTabController < TabController
       ),
 
 
-      merge_columns_request(@sheet_range[NET_HEADING_ROW, 0..(2 + @num_weeks)]),
-      center_text_request(@sheet_range[NET_HEADING_ROW, 0..(2 + @num_weeks)]),
-      set_outside_border_request(@sheet_range[NET_HEADING_ROW..NET_TOTAL_ROW, 0..(@num_weeks + 2)]),
-      right_align_text_request(@sheet_range[NET_WEEK_ROW, (1 + @num_weeks)..(2 + @num_weeks)]),
+      # right_align_text_request(@sheet_range[NET_WEEK_ROW, (1 + @num_weeks)..(2 + @num_weeks)]),
       set_bottom_border_request(@sheet_range[NET_WEEK_ROW, 0..(@num_weeks + 2)]),
-      set_right_border_request(@sheet_range[NET_WEEK_ROW..NET_TOTAL_ROW, 0]),
       set_left_right_border_request(@sheet_range[NET_WEEK_ROW..NET_TOTAL_ROW, 1 + @num_weeks]),
       bold_text_request(@sheet_range[NET_HEADING_ROW..NET_TOTAL_ROW, 0..(@num_weeks + 2)]),
       set_decimal_format_request(
