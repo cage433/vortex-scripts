38463fbab5ab3a3946ee2931b829f3a495783583
Alex McGuire <cage433@gmail.com>
2023-05-22 18:15:40 +0100

    Remove spike files from repo

--- spikes/test_exclusion.rb
+++
@@ -1,6 +0,0 @@
-require_relative '../controller/vol_rota_controller'
-require_relative '../env.rb'
-require_relative '../logging.rb'
-require 'date'
-
-airtable_events_personnel = VolunteerAirtableController.read_events_personnel(2023, 4)
